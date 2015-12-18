f67b650ce7c621465e16869ff6c97f3c0456bbe7
zhaoqian <qian.zhao3355@163.com>
2015-12-18 12:22:56 +0800

    commit the omited files
    Signed-off-by: zhaoqian <zhaoqian3355@163.com>

---
+++ ZQPhoenix/ZQNet.Presentation.Web.ZQPhoenix/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZQNet.Presentation.Web.ZQPhoenix.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome(string name,int id=1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello "+name+", NumTimes is : "+id);
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(int id,string name)
+        {
+            return HttpUtility.HtmlEncode("id: "+id+ ", Hello " + name);
+        }
+    }
+}
